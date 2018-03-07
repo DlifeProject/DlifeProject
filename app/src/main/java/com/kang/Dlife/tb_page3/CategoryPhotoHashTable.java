afdfc9b8ff12f908f4b9e8aaa48dbf210cba995f
Regan-Huang <irv278@gmail.com>
2018-03-07 14:23:07 +0800

    Regan 02/01 matchfriend

---
+++ app/src/main/java/com/kang/Dlife/tb_page3/CategoryPhotoHashTable.java
@@ -0,0 +1,29 @@
+package com.kang.Dlife.tb_page3;
+
+import com.kang.Dlife.Common;
+
+import java.util.Hashtable;
+
+/**
+ * Created by regan on 2018/3/5.
+ */
+
+class CategoryPhotoHashTable {
+
+    Hashtable<String,Integer> CategoryPotoTable = new Hashtable<String,Integer>();
+
+    CategoryPhotoHashTable(){
+        String[] categoryArray = Common.DEFAULTCATE;
+        int i = 0;
+
+        for (int i=0;i<categoryArray.length;i++){
+            if(!categoryArray[i].equals(Common.NONSHARECATE[0])) {
+                CategoryPotoTable.put(categoryArray[i], 0);
+            }
+        }
+    }
+
+    public Hashtable<String,Integer> getinit() {
+        return CategoryPotoTable;
+    }
+}
